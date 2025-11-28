68bdd5b787188db973ca9014480404ee58716ac8
Mikele Costantino <125467671+Blancos905@users.noreply.github.com>
2025-11-28 15:59:26 +0100

    first commit

---
+++ src/main/java/com/armaGest/identity_tenant_service/IdentityTenantServiceApplication.java
@@ -0,0 +1,13 @@
+package com.armaGest.identity_tenant_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityTenantServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityTenantServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/armaGest/identity_tenant_service/IdentityTenantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.armaGest.identity_tenant_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityTenantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
